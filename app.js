3d84131c0133a4b33195fce282e55bf3d7cd334a
damagermax <maxwelltakyi59@gmail.com>
2022-11-09 12:34:43 +0000

    init commit

---
+++ app.js
@@ -0,0 +1,15 @@
+
+let opnBtn=document.querySelector("#openBtn")
+
+
+
+
+opnBtn.addEventListener("click",()=>{
+    let mobileNav=document.querySelector("#topNav")
+     mobileNav.classList.toggle("active")
+    
+   
+})
+
+
+
